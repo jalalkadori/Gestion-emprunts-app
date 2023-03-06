e60bf95f5622bd4efc3bbecf276eb8361fd533f2
Jalal KADDOURI <jalalkadori2@gmail.com>
2023-03-06 13:40:54 +0100

    adding files

---
+++ login.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>LGM | Connection</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/75c6b1327b.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  </head>
+  <body>
+   
+    <nav class="navbar">
+        <div class="container-fluid">
+            <span class="navbar-brand mb-0 h1">
+                <img src="./logo/LGM.png" width="150">
+            </span>
+           
+        </div>
+    </nav>
+
+    <div class="container-fluid ">
+        <div class="container">
+            <div class="row mt-5">
+                <form action="#" method="post">
+                    
+                    <div class="my-3 text-center">
+                        <img src="./logo/user.png" class="circle p-5 rounded-circle" alt="" srcset="">
+                    </div>
+                    <div class="my-3 text-center">
+                        <h3>Admin</h3>
+                    </div>
+                    <div class="my-3">
+                        <input type="email" class="w-100 input-blue" placeholder="name@example.com">
+                    </div>
+                    <div class="my-3">
+                        <input type="password" class="w-100 input-blue" placeholder="Entrer votre mot de pass ...">
+                    </div>
+                    <div class="my-3">
+                        <button Class="w-100 blue">Connecter</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+  </body>
+</html>
